ac2a7f2031601a1d5452101ee6e0388a6d56bce7
ThisIsKhalid <iamkhalid.hs@gmail.com>
2025-01-15 14:25:21 +0600

    add new pages: AboutUs, Career, ContactUs, OurBlogs, OurProducts, OurServices, TermsConditions

---
+++ src/app/(common)/our-blogs/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function OurBlogsPage() {
+  return <div>OurBlogs</div>;
+}
---
+++ src/app/(common)/our-services/ux-ui-design/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function SingleServicePage() {
+  return <div>UxUiDesign</div>;
+}
