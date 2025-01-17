7fa3b510ae27f36c5c667290812b7a69afd84da8
ThisIsKhalid <iamkhalid.hs@gmail.com>
2025-01-17 08:59:46 +0600

    refactor CallToAction component for improved layout and responsiveness; adjust padding and positioning of background icons

--- src/components/shared/CallToAction/CallToAction.tsx
+++ src/components/shared/CallToAction/CallToAction.tsx
@@ -6,32 +6,32 @@ import React from "react";
 
 const CallToAction = () => {
   return (
-    <section className="container my-32">
-      <div className="bg-customGradient relative text-white rounded-[32px] flex flex-col items-center justify-center p-24 ">
+    <section className="container md:my-32 my-16">
+      <div className="bg-customGradient relative text-white rounded-[32px] flex flex-col items-center justify-center p-8 md:p-24 ">
         {/* Background Icons */}
-        <div className="absolute top-10 left-10">
+        <div className="absolute  md:top-1/2 top-14 lg:left-32 md:left-10 left-3 -translate-y-1/2">
+          <Image src={ctaEqual} alt="CTA Icon" width={55} height={55} />
+        </div>
+        <div className="absolute md:bottom-20 bottom-2 right-5 lg:right-32">
           <Image src={ctaEmail} alt="CTA Email" width={55} height={55} />
         </div>
-        <div className="absolute top-10 right-10">
+        <div className="absolute md:top-20 top-14 right-3">
           <Image src={ctaWp} alt="CTA WhatsApp" width={55} height={55} />
-        </div>
-        <div className="absolute bottom-10 right-20">
-          <Image src={ctaEqual} alt="CTA Icon" width={55} height={55} />
         </div>
 
         {/* Content */}
-        <div className="p-9 flex flex-col items-center justify-center">
-          <h2 className="text-center text-[64px] leading-[71.68px] tracking-[-0.02em] font-light">
+        <div className="md:p-9 p-0 flex flex-col items-center justify-center">
+          <h2 className="text-center text-5xl md:text-[64px] md:leading-[71.68px] tracking-[-0.02em] font-light">
             <span className="font-bold">READY</span> TO ELEVATE <br /> YOUR{" "}
             <span className="font-bold">BRAND?</span>
           </h2>
 
-          <p className="font-satoshi text-[18px] font-bold leading-[28px] text-center text-white/60 max-w-2xl my-10">
+          <p className="font-satoshi text-[18px] font-bold leading-[28px] text-center text-white/60 md:max-w-2xl w-full my-10">
             Ready to take your business to the next level? At Vyazen, we’re here
             to turn your ideas into impactful digital solutions.
           </p>
 
-          <button className=" bg-yellow-primary text-black px-[33px] py-[12px] hover:bg-yellow-400 transition text-lg font-semibold rounded-full">
+          <button className=" bg-yellow-primary text-black px-[33px] py-[12px] hover:bg-yellow-400 transition text-lg font-semibold rounded-full text-nowrap">
             Schedule A Call
           </button>
         </div>
