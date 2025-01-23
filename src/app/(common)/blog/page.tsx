242ab63ff0e09189fea8f7569269dc98259ec9db
SobuZ391 <monowerhossainsobuz@gmail.com>
2025-01-24 02:24:50 +0600

    blog id page design 99% okay and in reponsesiveness not responsive

---
+++ src/app/(common)/blog/page.tsx
@@ -0,0 +1,30 @@
+import Blogs from "@/components/pages/Blogs/page";
+import CallToAction from "@/components/shared/CallToAction/CallToAction";
+import FAQ from "@/components/shared/FAQ/FAQ";
+import PageHeader from "@/components/shared/PageHeader/PageHeader";
+import SectionHeader from "@/components/shared/SectionHeader/SectionHeader";
+
+export default function blog() {
+    return <div>
+    <PageHeader title="Contact Us" subtitle="Contact Us" />
+    <div className="container  md:mt-[140px] sm:mt-[70px] mt-[50px]">
+      <SectionHeader
+        subtitle="GET IN TOUCH"
+        subtitleClass="white"
+        title={
+          <>
+            <span className="font-bold">Need </span> Assistance
+             <br />
+            Want to <span className="font-bold"> Collaborate!</span>
+          </>
+        }
+        titleClass="md:text-[64px] md:leading-[74px]"
+        align="center"
+      />
+    </div>
+   <Blogs/>
+    <FAQ />
+    <CallToAction />
+  </div>;
+  }
+  
