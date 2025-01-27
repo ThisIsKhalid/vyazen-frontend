90a59d918ccc708eb7c2ecf0d08109b98932f644
ThisIsKhalid <iamkhalid.hs@gmail.com>
2025-01-27 23:45:34 +0600

    refactor: enhance HeroSection layout and styling for improved responsiveness

--- src/components/pages/home/HeroSection.tsx
+++ src/components/pages/home/HeroSection.tsx
@@ -10,55 +10,53 @@ export default function HeroSection() {
       </div>
 
       {/* Content */}
-      <div className="relative container md:py-[140px]">
-        <div className="flex">
-          {/* Left Content */}
-          <div className="text-center lg:text-left">
-            <div className="text-center md:text-left">
-              <span className="py-1.5 px-8 rounded-full font-satoshi font-bold text-base tracking-[8px] uppercase shadow-[1px_3px_1px_0px_rgba(255,255,255,0.2)] text-nowrap bg-[#FFFFFF0F] text-white">
-                Digital Agency
-              </span>
-            </div>
-            {/* Main Heading */}
-            <h2 className="font-light text-2xl tracking-[-0.02em] md:text-left text-center uppercase md:text-[92px] md:leading-[103px] text-white my-[52px]">
-              <span className="font-bold">our </span>Creative digital{" "}
-              <span className="font-bold">agency.</span>
-            </h2>
-
-            {/* Welcome Text */}
-            <p className="text-white/60 max-w-[732px] font-bold font-satoshi text-sm sm:text-base lg:text-lg">
-              Welcome to Vyazen, where innovation drives transformation. We
-              specialize in delivering tailored solutions that empower
-              businesses to achieve their full potential.
-            </p>
-
-            {/* Contact Button */}
-            <div className="mt-[52px]">
-              <button className="bg-yellow-primary text-black px-10 py-4 rounded-full hover:bg-yellow-400 transition font-bold text-base font-satoshi">
-                Contact With Us
-              </button>
-            </div>
+      <div className="relative container xl:py-[140px] py-[70px] flex xl:flex-row flex-col gap-x-5 gap-y-10">
+        {/* Left Content */}
+        <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left">
+            <span className="py-1.5 px-8 rounded-full font-satoshi font-bold text-base tracking-[8px] uppercase shadow-[1px_3px_1px_0px_rgba(255,255,255,0.2)] text-nowrap bg-[#FFFFFF0F] text-white">
+              Digital Agency
+            </span>
           </div>
+          {/* Main Heading */}
+          <h2 className="font-light text-5xl tracking-[-0.02em] uppercase md:text-[92px] md:leading-[103px] text-white md:my-[52px] my-10">
+            <span className="font-bold">our </span>Creative digital
+            <span className="font-bold"> agency.</span>
+          </h2>
 
-          {/* Right Image */}
-          <div className="relative flex items-center justify-center">
-            {/* Yellow Background */}
-            <div className="absolute inset-0 bg-gradient-to-br from-yellow-primary to-transparent rounded-2xl blur-[80px]"></div>
+          {/* Welcome Text */}
+          <p className="text-white/60 max-w-[732px] mx-auto xl:mx-0 font-bold font-satoshi text-sm sm:text-base lg:text-lg">
+            Welcome to Vyazen, where innovation drives transformation. We
+            specialize in delivering tailored solutions that empower businesses
+            to achieve their full potential.
+          </p>
 
-            {/* Image Container */}
-            <div className="relative w-[280px] sm:w-[360px] lg:w-[420px] h-[240px] sm:h-[300px] lg:h-[355px] rounded-3xl bg-yellow-500">
-              <iframe
-                width="420"
-                height="355"
-                src="https://www.youtube.com/embed/qWNfz8yC_eM?si=OFHkx5a3U_eIGIBP"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-                className="w-full h-full rounded-3xl"
-              ></iframe>
-            </div>
+          {/* Contact Button */}
+          <div className="md:mt-[52px] mt-10">
+            <button className="bg-yellow-primary text-black px-10 py-4 rounded-full hover:bg-yellow-400 transition font-bold text-base font-satoshi">
+              Contact With Us
+            </button>
+          </div>
+        </div>
+
+        {/* Right Image */}
+        <div className="relative flex items-center justify-center">
+          {/* Yellow Background */}
+          <div className="absolute inset-0 bg-gradient-to-br from-yellow-primary to-transparent rounded-2xl blur-[80px]"></div>
+
+          {/* Image Container */}
+          <div className="relative w-[280px] sm:w-[360px] lg:w-[420px] h-[240px] sm:h-[300px] lg:h-[355px] rounded-3xl bg-yellow-500">
+            <iframe
+              width="420"
+              height="355"
+              src="https://www.youtube.com/embed/qWNfz8yC_eM?si=OFHkx5a3U_eIGIBP"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              className="w-full h-full rounded-3xl"
+            ></iframe>
           </div>
         </div>
       </div>
