41c237c257072167dd204ddea04efc49d827d577
SobuZ391 <monowerhossainsobuz@gmail.com>
2025-01-29 22:43:51 +0600

    created privacy policy page and made it responsive

---
+++ src/components/pages/privacyPolicy/privacyAndPolicy.tsx
@@ -0,0 +1,289 @@
+export default function PrivacyPolicy() {
+  return (
+    <main className="max-w-3xl mx-auto px-4">
+      <section className="space-y-12">
+        <div className="space-y-6">
+          <h1 className="md:text-4xl text-xl font-bold">
+            What Information Do We Collect?
+          </h1>
+          <p className="font-satoshi font-medium text-black-secondary leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="font-satoshi font-medium text-black-secondary leading-relaxed">
+            Eget purus aenean sit risus. Arcu, aliquam eget et viverra risus
+            purus. Commodo fames tristique dui pharetra elit aliquet morbi. Eget
+            consectetur risus nunc lorem sit consequat aliquet. Dolor velit
+            consecte tur etiam scelerisque. Integer interdum sodales scelerisque
+            diam massa quam sit quis. Sed et dui a nam pulvinar. Tristique
+            justo, donec lectus vitae, cursus ligula ridiculus lacus, tincidunt.
+            Diam dictumst faucib us dui aliquet aenean nascetur feugiat leo.
+            Etiam dignissim orci dignissim vitae.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h1 className="md:text-4xl text-xl font-bold">
+            Use of Information.
+          </h1>
+          <p className="text-black-secondary font-satoshi font-medium leading-relaxed  md:pt-4 ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Ada wati nora dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <ul className="list-none space-y-3 pt-[10px] font-satoshi font-medium">
+            <li className="flex items-start ">
+              <span className="w-2.5 h-2.5 bg-yellow-primary rotate-45 mr-3 mt-2 block"></span>
+              <p>
+                Diam dictumst faucibus dui aliquet acenean nascetur feugiat leo
+                Etiam dignissim orci dignissim.
+              </p>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2.5 h-2.5 bg-yellow-primary rotate-45 mr-3 mt-2 block"></span>
+              <p>
+                Blandit dignissim nulla varius tristique a sed integer ut tempor
+                Diam dictumst faucibus.
+              </p>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2.5 h-2.5 bg-yellow-primary rotate-45 mr-3 mt-2 block"></span>
+              <p>
+                Ed nam vulputate pellentesque quis. Varius a, nunc faucibus
+                proin elementum id odio auctor.
+              </p>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2.5 h-2.5 bg-yellow-primary rotate-45 mr-3 mt-2 block"></span>
+              <p>
+                Integer interdum sodales scelerisque diam massa quam sit quis,
+                sed et dui a nam pulvinar.
+              </p>
+            </li>
+            <li className="flex items-start">
+              <span className="w-2.5 h-2.5 bg-yellow-primary rotate-45 mr-3 mt-1 block"></span>
+              <p>
+                Pretium consectetur scelerisque blandit habitasse non
+                ullamcorper enim.
+              </p>
+            </li>
+          </ul>
+          <p className="text-black-secondary py-[10px] font-satoshi font-medium leading-relaxed text-now mt-6">
+            Eget purus aenean sit risus. Arcu, aliquam eget et viverra risus
+            purus. Commodo fames tristique dui pharetra elit aliquet morbi. Eget
+            consectetur risus nunc lorem sit consequat aliquet. Dolor velit
+            consecte tur etiam scelerisque. Integer interdum sodales scelerisque
+            diam massa quam sit quis. Sed et dui a nam pulvinar. Tristique
+            justo, donec lectus vitae, cursus ligula ridiculus lacus, tincidunt.
+            Diam dictumst faucib us dui aliquet aenean nascetur feugiat leo.
+            Etiam dignissim orci dignissim vitae.
+          </p>
+          <p className="text-black-secondary font-satoshi font-medium leading-relaxed">
+            aliquam eget et viverra risus purus. Commodo fames tristique dui
+            pharetra elit aliquet morbi. Eget consectetur risus nunc lorem sit
+            consequat aliquet. Dolor velit consecte tur etiam scelerisque.
+            Integer interdum sodales scelerisque diam massa quam sit quis. Sed
+            et dui a nam pulvinar. Tristique justo, donec lectus vitae, cursus
+            ligula ridiculus lacus, tincidunt. Diam dictumst faucib us dui
+            aliquet aenean nascetur feugiat leo. Etiam dignissim.
+          </p>
+        </div>
+
+        <div className="space-y-4">
+          <h2 className="md:text-4xl text-xl font-bold">
+            Security and Third Parties.
+          </h2>
+          <p className="text-black-secondary font-satoshi md:py-2 font-medium leading-relaxed">
+            Blandit dignissim nulla varius tristique a sed integer ut tempor.
+            augue interdum semper bibendum amet sed. dis in et ultrices tortor
+            et tellus. habitant ornare senean nascetur. Ut ullamcorper nulla
+            enim egestas viverra et id aliquot faucibus rhoncus a. sollicitudin
+            nisi nulla tempor pretium lorem at mauris faucibus pulvinar.
+          </p>
+          <ul className="list-none  space-y-4">
+            <li className="  leading-relaxed pl-2">
+              <span className=" font-bold font-satoshi">
+                1. Egit purus senean sit risus.{" "}
+              </span>
+              <span className="text-black-secondary font-satoshi font-medium">
+                arcu, aliquam quis et viverra nulla purus, commodo fames
+                tristique do gravida elit aliquot metus aliquam quis et viverra
+                risus purus.
+              </span>
+            </li>
+            <li className=" leading-relaxed pl-2">
+              <span className="font-bold font-satoshi">
+                2.Commodo fames tristique do pharetra elit aliquot morbi.{" "}
+              </span>
+              <span className="text-black-secondary font-satoshi font-medium0">
+                et consectetur nulla nunc lorem sit consequat aliquot, dolor
+                velit consectetur etiam scelerisque. integer interdum sodales
+                scelerisque diam massa quam et.
+              </span>
+            </li>
+            <li className=" leading-relaxed pl-2">
+              <span className="font-bold font-satoshi">
+                3.Risique justo, donec lectus vita.,{" "}
+              </span>
+              <span className="text-black-secondary font-satoshi font-medium">
+                {" "}
+                cursus ligula ridiculus lacus, tincidunt, diam dictumst faucibus
+                dui aliquot senean natoque feugiat leo, etiam dignissim orci
+                dignissim vitae.
+              </span>
+            </li>
+            <li className=" leading-relaxed pl-2">
+              <span className="font-bold font-satoshi">
+                4.Nullam morbi ornare tellus felis.{" "}
+              </span>
+              <span className="text-black-secondary font-satoshi font-medium">
+                morbi senectus velit nimet a, pellentesque tincidunt. in
+                consectetur scelerisque consectetur lectus ut sit diam.
+                pellentesque quam fringilla in egestas et consequat.
+              </span>
+            </li>
+          </ul>
+          <p className="text-black-secondary font-satoshi font-medium leading-relaxed">
+            Dignissim nulla varius tristique a sed integer ut tempor. augue
+            interdum semper bibendum amet amet sed. dis in et ultrices tortor et
+            tellus. habitant ornare senean nascetur. Ut ullamcorper nulla enim
+            egestas viverra et id aliquot faucibus rhoncus a. sollicitudin nisi
+            nulla tempor pretium lorem at mauris faucibus pulvinar.nunc,
+            suspendisse consequat libero, pellentesque blandit nulla suscipit
+            dolor senectus tortor non rhoncus a duis. pharetra ipsum massa
+            integer sit feugiat.egit purus senean sit risus, arcu, aliquam quis
+            et viverra nulla purus, commodo fames tristique do gravida elit
+            aliquot metus, quid vestibulum. risus nunc lorem sit consequat
+            aliquot.
+          </p>
+          <ul className="space-y-2 font-satoshi font-medium text-black px-2">
+            <li className="flex items-start gap-2">
+              <span className="mt-2 block w-1.5 h-1.5 rounded-full bg-black flex-shrink-0" />
+              Blandit dignissim nulla varius tristique a sed integer ut tempor.
+            </li>
+            <li className="flex items-start gap-2">
+              <span className="mt-2 block w-1.5 h-1.5 rounded-full bg-black flex-shrink-0" />
+              Id ipsum mi tempor eget, pretium consectetur scelerisque blandit
+              habitasse non ullamcorper enim
+            </li>
+            <li className="flex items-start gap-2">
+              <span className="mt-2 block w-1.5 h-1.5 rounded-full bg-black flex-shrink-0" />
+              Diam quam id et, tempus massa, sed nam vulputate pellentesque
+              quis, velit a, nunc facilisis.
+            </li>
+          </ul>
+        </div>
+
+        <div className="space-y-4 ">
+          <h2 className="md:text-4xl text-xl font-bold">Use of Cookies.</h2>
+          <p className="font-satoshi md:py-2 font-medium text-black-secondary  ">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, <br />
+            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis quasi
+            architecto beatae vitae dicta sunt
+            <br />
+            explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+            aut odit aut fugit, sed quia <br />
+            consequuntur magni dolores eos qui ratione voluptatem eaque
+            nesciunt. Eleifend orci sed pulvinar <br />
+            blandit aliquam nisl sed libero amet. Etiam lectus sed enim eu
+            commodo nisi. Tellus vehicula arcu sit egestas porttitor quis
+            faucibus.
+          </p>
+
+          <ul className="space-y-4">
+            <li className="flex items-start gap-2 font-satoshi">
+              <span className="mt-2 block w-2 h-2 rounded-full bg-black flex-shrink-0"></span>
+              <div>
+                <span className="font-bold font-satoshi ">
+                  Blandit dignissim.{" "}
+                </span>
+                <span className="text-black-secondary font-medium ">
+                  nulla varius tristique a sed integer ut tempor.
+                </span>
+              </div>
+            </li>
+            <li className="flex items-start gap-2 font-satoshi">
+              <span className="mt-2 block w-2 h-2 rounded-full bg-black flex-shrink-0"></span>
+              <div>
+                <span className="font-bold font-satoshi ">
+                  Id ipsum mi tempor.{" "}
+                </span>
+                <span className="text-black-secondary font-medium">
+                  eget pretium consectetur scelerisque blandit habitasse non
+                  ullamcorper enim.
+                </span>
+              </div>
+            </li>
+            <li className="flex items-start gap-2 font-satoshi">
+              <span className="mt-2 block w-2 h-2 rounded-full bg-black flex-shrink-0"></span>
+              <div>
+                <span className="font-bold font-satoshi ">
+                  Diam quam id et, tempus massa.{" "}
+                </span>
+                <span className="text-black-secondary font-medium">
+                  sed nam vulputate pellentesque quis. Varius a, nunc faucibus.
+                </span>
+              </div>
+            </li>
+            <li className="flex items-start gap-2 font-satoshi">
+              <span className="mt-2 block w-2 h-2 rounded-full bg-black flex-shrink-0"></span>
+              <div>
+                <span className="font-bold font-satoshi ">
+                  Neque rhoncus in amet ipsum.
+                </span>
+                <span className="text-black-secondary font-medium">
+                  eget lacus odio. Viverra mus eu amet risus tempor pretium
+                  habitant et.
+                </span>
+              </div>
+            </li>
+            <li className="flex items-start gap-2 font-satoshi">
+              <span className="mt-2 block w-2 h-2 rounded-full bg-black flex-shrink-0"></span>
+              <div>
+                <span className="font-bold font-satoshi ">
+                  Etiam lectus sed enim eu.{" "}
+                </span>
+                <span className="text-black-secondary font-medium">
+                  commodo nisi. Tellus vehicula arcu sit egestas porttitor quis
+                  faucibus.
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-4">
+          <h2 className="md:text-4xl text-xl  font-bold">
+            The rights of Users.
+          </h2>
+          <p className="text-black-secondary md:py-2 font-satoshi font-medium leading-relaxed">
+            Malesuada tortor at mattis semper aenean. Tristique nisi
+            pellentesque fringilla ipsum sed amet dui amet mattis. Eleifend orci
+            sed pulvinar blandit aliquam nisl sed libero amet. Etiam lectus sed
+            enim eu commodo nisi. Tellus vehicula arcu egestas porttitor quis
+            faucibus. Sit lacus id pretium malesuada velit.
+          </p>
+          <p className="text-black-secondary font-satoshi font-medium leading-relaxed">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem eaque nesciunt.eleifend orci sed pulvinar
+            blandit aliquam nisi sed libero amet. diam lectus sed enim eu
+            commodo nisi. tellus vehicula arcu et egestas porttitor quis
+            faucibus.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
